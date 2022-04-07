a4be3dd9a959a0d095b7332c9a5a15d57924e4af
Umidjon <mrumidjon2020@gmail.com>
2022-04-07 05:17:10 +0500

    A string of numbers is given. Find how many even numbers there are and return. 01

--- While07.py
+++ While07.py
@@ -1,7 +1,6 @@
 def main(s):
     """
-    A string of numbers is given. Find how many even numbers there are and return.
-    Args:
+g    Args:
         s: str
     Returns:
         int: return answer
@@ -10,7 +9,7 @@ def main(s):
     k = 0
 
     while i < len(s):
-        son = int(i)
+        son = int(s[i])
         if son % 2 == 0:
             k += 1
         i += 1
