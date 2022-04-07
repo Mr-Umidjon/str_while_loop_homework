8962e6ac57b86416167930f4c4d4833dae8e5506
Umidjon <mrumidjon2020@gmail.com>
2022-04-07 05:11:06 +0500

    A string of numbers is given. Find and return the sum of all odd numbers. 01

--- While10.py
+++ While10.py
@@ -6,4 +6,15 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    k = 0
+
+    while i < len(s):
+        son = int(s[i])
+        if son % 2 == 1:
+            k += son
+        i += 1
+    return k
+
+
+print(main('1234'))
